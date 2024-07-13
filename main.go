d14738067f7b11a611fa5d3720107682fc5558b3
novak <david.novak@nokia.com>
2024-07-13 14:37:56 +0200

    save game ;)

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func main() {
 		Addr:    "127.0.0.1:8081",
 		Handler: mux, // your ServeMux that you created earlier
 	}
+	mux.Handle("/", http.FileServer(http.Dir("/root/bootDotDev/Chirpy/")))
 	if err := server.ListenAndServe(); err != nil {
 		log.Fatalf("Server failed to start: %v", err)
 	}
